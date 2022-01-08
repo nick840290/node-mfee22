6b0166f38a9e3b669f0afc7f7ab5ed85e735ad6b
nick840290 <nick840290@gmail.com>
2022-01-09 01:58:00 +0800

    add callback content

---
+++ callback/readfile2.js
@@ -0,0 +1,19 @@
+const { readFile } = require("fs");
+
+let readFilePromise = new Promise((resolve, reject) => {
+  readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(data);
+  });
+});
+
+readFilePromise
+  .then((data) => {
+    console.log(`這裡是test.txt的內容:${data}`);
+  })
+  .catch((err) => {
+    console.log(`讀取錯誤, ${err}`);
+  });
