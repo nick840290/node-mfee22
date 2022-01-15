a9d5c9ab7a20a047be76f1d83d5ac20db828a1c7
nick840290 <nick840290@gmail.com>
2022-01-15 12:34:40 +0800

    [add]crawler4 今日日期

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -6,24 +6,14 @@ const { readFile } = require("fs/promises");
 (async () => {
   try {
 
-        //   console.log(` ${result}`);
           let stockNo =  await readFile("stock.txt", "utf-8");;
 
+          //今日日期
           var fullDate = new Date();
           var yyyy = fullDate.getFullYear();
           var MM = (fullDate.getMonth() + 1) >= 10 ? (fullDate.getMonth() + 1) : ("0" + (fullDate.getMonth() + 1));
           var dd = fullDate.getDate() < 10 ? ("0"+fullDate.getDate()) : fullDate.getDate();     
           let queryDate =  yyyy+MM+dd;
-
-    // 根據變數去抓取資料
-    // let stockNo = 2330;
-
-    // let response = await axios.get(
-    //   `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${stockNo}`
-    // );
-
-
-
 
     let response = await axios.get(
       "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
